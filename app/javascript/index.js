43e297e2595e1397a80a02bfc6ec5a8c98416ad3
Steven Torrence <storrence88@gmail.com>
2021-01-25 21:46:35 -0600

    Animate the hamburger menu icon

--- app/javascript/index.js
+++ app/javascript/index.js
@@ -1,1 +1,10 @@
 console.log('Hello, world!');
+
+const btn = document.querySelector('#btnHamburger');
+btn.addEventListener('click', () => {
+  if (btn.classList.contains('open')) {
+    btn.classList.remove('open');
+  } else {
+    btn.classList.add('open');
+  }
+});
